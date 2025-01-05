1bdb159afb103d3c08c56c1ef378bcfdc3c3e48f
Deepanshu Rajput <43910264+deepanshu03091995@users.noreply.github.com>
2025-01-05 21:55:09 +0530

    consumer code pushed to the github

---
+++ docker/consumer/kafka_consumer.py
@@ -0,0 +1,26 @@
+import hdfs
+from kafka import KafkaConsumer
+from json import loads
+import time
+
+time.sleep(50)
+
+
+try:
+
+    consumer = KafkaConsumer(
+        'hb',
+        bootstrap_servers=['kafka:29092'],
+        auto_offset_reset='earliest',
+        enable_auto_commit=True,
+        group_id='my-group',
+        value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+except:
+  raise Exception('kafka connect error')
+
+
+for message in consumer:
+    message = message.value
+    hdfs.write_to_hdfs(str(message))
+    print(message)
